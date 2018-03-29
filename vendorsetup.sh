70c651e801500c99aadec3f0f4879da7f08f86df
ananjaser1211 <ananjaser@gmail.com>
2018-03-29 00:08:55 +0000

    j5y17lte: Add -eng builds for LOS/RR

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 add_lunch_combo lineage_j5y17lte-userdebug
+add_lunch_combo lineage_j5y17lte-eng
 add_lunch_combo rr_j5y17lte-userdebug
+add_lunch_combo rr_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-eng
 add_lunch_combo aosp_j5y17lte-userdebug
