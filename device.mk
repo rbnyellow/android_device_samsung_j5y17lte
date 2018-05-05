b1a3f9cdfb2a6948d5b455dd3060a94018af79f5
ananjaser1211 <ananjaser@gmail.com>
2018-05-05 18:22:20 +0000

    j5y17te: use HWUI Values manually for better compatibility with other AOSP roms

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 # Dalvik Heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
-# Hwui Memory
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
-
 # Inherit from Exynos7870-common
 $(call inherit-product, device/samsung/exynos7870-common/device-common.mk)
 
