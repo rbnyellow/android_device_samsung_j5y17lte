7fb541d6b5425085168d847901bdd03a9afef9b7
Robin <cool.guichard@gmail.com>
2020-05-16 19:21:11 +0200

    add maru for j5y17lte

---
+++ maru_j5y17lte.mk
@@ -0,0 +1,29 @@
+#
+# Copyright 2018 The Maru OS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# -----------------------------------------------------------------------------
+# Include LineageOS stuff
+
+$(call inherit-product, device/samsung/j5y17lte/lineage.mk)
+
+# -----------------------------------------------------------------------------
+# Include Maru stuff
+
+$(call inherit-product, vendor/maruos/device-maru.mk)
+$(call inherit-product, vendor/maruos/BoardConfigVendor.mk)
+
+PRODUCT_NAME := maru_j5y17lte
+PRODUCT_MODEL := Maru on the Galaxy J5 2017
